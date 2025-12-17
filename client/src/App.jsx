f916360cb5c83a057c282889aca4dd0386623dfd
Dragana Begojev <dragana.begojev@gmail.com>
2025-12-17 07:46:41 +0100

    Build Shared Components
    - Create Header.jsx in /components directory
    - Import the Header component in the App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
   );
 };
 
 export default App;
+
 
