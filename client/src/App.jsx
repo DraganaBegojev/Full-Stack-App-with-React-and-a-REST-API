b7870c97a31bd5a5a03be205a107bff77b6f30d5
Dragana Begojev <dragana.begojev@gmail.com>
2025-12-22 11:25:25 +0100

    Create Course Screen
    - Set up a Route for the create course screen that renders the <CreateCourse /> component
    - In the handleSubmit function, send a POST request to the REST API's /api/courses endpoint with the user id, course title, description, estimated time, and materials needed in the body of the request
    - After successfully creating the new course, redirect the user to the "Courses" or "Course Detail" screen for the newly created course

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
+import CreateCourse from './components/CreateCourse';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/courses/:id" element={<CourseDetail />} />
         <Route path="/sign-in" element={<UserSignIn />} />
         <Route path="/sign-up" element={<UserSignUp />} />
+        <Route path="/courses/create" element={<CreateCourse />} />
       </Routes>
     </>
   );
