0221b7b9be1f2404f8fe957ff8d731f2e3db307f
Dragana Begojev <dragana.begojev@gmail.com>
2025-12-24 10:45:58 +0100

    Delete Course Functionality
    - In the <CourseDetail /> component, add an onClick event handler to the "Delete Course" button
    - When the "Delete Course" button is clicked, send a DELETE request to the REST API's /api/courses/:id endpoint
    - After successfully deleting the course, redirect the user to the "Courses" screen (homepage)

--- client/src/components/CourseDetail.jsx
+++ client/src/components/CourseDetail.jsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
+import AuthContext from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const CourseDetail = () => {
   const { id } = useParams();
   const [course, setCourse] = useState(null);
+  const { authUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/courses/${id}`)
@@ -15,6 +19,41 @@ const CourseDetail = () => {
 
   if (!course) return null;
 
+  // delete course function
+
+  const handleDelete = async () => {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this course?'
+    );
+
+    if (!confirmed) return;
+
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/courses/${id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization:
+              'Basic ' +
+              btoa(
+                `${authUser.credentials.emailAddress}:${authUser.credentials.password}`
+              ),
+          },
+        }
+      );
+
+        if (response.status === 204) {
+          navigate('/');
+        } else {
+          throw new Error();
+        }
+      } catch {
+        alert('Something went wrong while deleting the course.');
+      }
+  };
+
+
   return (
     <main>
       <div className="actions--bar">
@@ -22,7 +61,12 @@ const CourseDetail = () => {
           <Link className="button" to={`/courses/${id}/update`}>
             Update Course
           </Link>
-          <button className="button">Delete Course</button>
+          {authUser && authUser.id === course.userId && (
+            <button className="button" onClick={handleDelete}>
+              Delete Course
+            </button>
+          )}
+
           <Link className="button button-secondary" to="/">
             Return to List
           </Link>
